7811381bab5d45622c03af7abd8d29c286421666
paulvandyke <paulvandyke@paulvandykes-MacBook-Pro.local>
2025-11-05 12:23:52 -0500

    removed todos and boiler plate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./app.css";
 import AuthenticatorWrapper from "./AuthenticatorWrapper";
 import "@aws-amplify/ui-react/styles.css";
--- app/page.tsx
+++ app/page.tsx
@@ -15,41 +15,10 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const { user, signOut } = useAuthenticator();
-
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
+  console.log(JSON.stringify(user, null, 2));
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      <h1>{user?.signInDetails?.loginId} Is the current Logged in user</h1>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
